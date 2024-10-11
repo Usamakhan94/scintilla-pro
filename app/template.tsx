2fe3cf9230e3a3f9806d1c290268598e0a68e662
Usamakhan94 <usama_khan1994@live.com>
2024-10-12 04:39:31 +0500

    Smooth scroller added

---
+++ app/template.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+  return <>{children}</>;
+}
